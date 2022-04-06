8613f756326bc4aaa5c61ae51df506ae26a093ff
Monster0506 <traklovits@gmail.com>
2022-04-05 20:45:44 -0400

    Initial Commit

---
+++ DynamicArray/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+	public static void main(String[] args) {
+		DynamicArray dynamicArray = new DynamicArray();
+		dynamicArray.add(1);
+		dynamicArray.add(5);
+		dynamicArray.add(3);
+		dynamicArray.add(6);
+		dynamicArray.add(5);
+		dynamicArray.add(8);
+		dynamicArray.add(7);
+		System.out.println(dynamicArray.max());
+		System.out.println(dynamicArray.min());
+		System.out.println(dynamicArray);
+	    }
+}
